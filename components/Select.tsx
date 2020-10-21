9ad9562ba74f65ad54a661bad8b009b8268208b1
Mikk Martin <mikk.martin@gmail.com>
2020-10-21 14:27:34 +0300

    dark theme stuff

--- components/Select.tsx
+++ components/Select.tsx
@@ -5,7 +5,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 import { StylesProvider } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
 import InputBase from "@material-ui/core/InputBase";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, useTheme } from "styled-components";
 
 type Value = number | string;
 type SelectProps = {
@@ -14,12 +14,19 @@ type SelectProps = {
   fullWidth?: boolean;
 };
 
+export type Theme = {
+  dark?: boolean;
+};
+
 export const Select: FC<SelectProps> = ({
   children,
   value,
   onChange,
   fullWidth
 }) => {
+  const theme = useTheme();
+  console.log(theme);
+
   return (
     <StylesProvider injectFirst>
       <StyledFormControl fullWidth={fullWidth}>
@@ -54,21 +61,15 @@ type OptionProps = {
   disabled?: boolean;
 };
 
-export const Option: FC<OptionProps> = ({ children, ...props }) => {
-  return <StyledItem {...props}>{children}</StyledItem>;
-};
-
 const StyledInput = styled(InputBase)`
   .MuiSelect-root {
     font-family: "Manrope";
     letter-spacing: 1px;
     box-sizing: border-box;
-    background: orange;
     height: 54px;
     text-align: left;
     padding: 16px;
     padding-right: 54px;
-    background: rgba(0, 0, 0, 0.04);
     border: 2px solid transparent;
     display: flex;
     width: 100%;
@@ -83,14 +84,24 @@ const StyledInput = styled(InputBase)`
         flex-shrink: 0;
       }
     }
-    color: ${(props) =>
-      props.value === "" ? "rgba(0, 0, 0, 0.35)" : "rgba(0, 0, 0, 1)"};
+    background: ${({ theme }) =>
+      theme.dark ? "rgba(255, 255, 255, 0.1)" : "rgba(0, 0, 0, 0.05)"};
+    color: ${({ value, theme }) =>
+      theme.dark
+        ? value === ""
+          ? "rgba(255, 255, 255, 0.35)"
+          : "rgba(255, 255, 255, 1)"
+        : value === ""
+        ? "rgba(0, 0, 0, 0.35)"
+        : "rgba(0, 0, 0, 1)"};
     &:hover {
-      background: rgba(0, 0, 0, 0.08);
+      background: ${({ theme }) =>
+        theme.dark ? "rgba(255, 255, 255, 0.1)" : "rgba(0, 0, 0, 0.2)"};
     }
     &:focus {
-      background: transparent;
-      border: 2px solid #005fff;
+      background: rgba(255, 255, 255, 0.1);
+      border: 2px solid white;
+      box-shadow: 0 0 0 2px rgba(255, 255, 255, 0.65);
     }
   }
   .MuiSelect-root,
@@ -102,11 +113,16 @@ const StyledInput = styled(InputBase)`
 const StyledItem = styled(MenuItem)`
   font-family: "Manrope";
   letter-spacing: 1px;
+  min-height: 40px;
   display: flex;
   width: 100%;
   justify-content: space-between;
   transition: 0s;
 `;
+
+export const Option: FC<OptionProps> = ({ children, ...props }) => {
+  return <StyledItem {...props}>{children}</StyledItem>;
+};
 
 const StyledFormControl = styled(FormControl)``;
 const SvgContainer = styled.svg`
@@ -115,6 +131,13 @@ const SvgContainer = styled.svg`
   height: 100%;
   width: 40px;
   top: 0;
+  #line {
+    stroke: ${(props) =>
+      props.theme?.dark ? "rgba(255, 255, 255, 0.1)" : "rgba(0, 0, 0, 0.1)"};
+  }
+  #chevron {
+    stroke: ${(props) => (props.theme?.dark ? "white" : "black")};
+  }
   &.MuiSelect-iconOpen {
     transform: scaleY(-1);
   }
@@ -124,21 +147,20 @@ const Chevron: FC = (props) => {
   return (
     <SvgContainer {...props} width="44" height="34" viewBox="0 0 44 34">
       <path
+        id="line"
         d="M 0.5 0.5 L 0.5 33.5"
         fill="transparent"
-        stroke="rgba(0, 0, 0, 0.1)"
         strokeLinecap="square"
-      ></path>
-
+      />
       <path
+        id="chevron"
         d="M 16 14 L 22 20 L 28 14"
         fill="transparent"
         strokeWidth="1.5"
-        stroke="rgb(0,0,0)"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeDasharray=""
-      ></path>
+      />
     </SvgContainer>
   );
 };
@@ -146,9 +168,10 @@ const Chevron: FC = (props) => {
 const GlobalStyles = createGlobalStyle`
   .MuiMenu-paper {
     box-sizing: border-box;
-    background: rgba(220, 220, 220, 0.3);
+    background: rgba(0, 0, 0, 0.3);
     border-radius: 12px;
     backdrop-filter: blur(40px);
     box-shadow: 0 10px 60px -5px rgba(0,0,0,0.2), inset 0 0 0 1px rgba(255,255,255,0.21);
+    color: white;
   }
 `;
